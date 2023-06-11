96ae57b4be740ec80ec313264e2f0c79b98e1b6d
nicklelate <101852708+nicklelate@users.noreply.github.com>
2023-06-11 15:57:12 +0700

    Update cap_and_count.py

--- cap_and_count.py
+++ cap_and_count.py
@@ -1,12 +1,13 @@
 import cv2
 from cv2 import *
-import numpy as np
-import matplotlib.pyplot as plt
 import cvlib as cv
-from cvlib.object_detection import draw_bbox
-from numpy.lib.polynomial import poly
 import datetime
+import time
+from firebase import firebase
+# python -m pip install
 
+url = 'https://cmudigitaltwin-default-rtdb.asia-southeast1.firebasedatabase.app/'
+messenger = firebase.FirebaseApplication(url)
 b = True
 run = 1
 while b:
@@ -14,21 +15,21 @@ while b:
     cam = cv2.VideoCapture(cam_port)
     result, image = cam.read()
     x = datetime.datetime.now()
-    y = x.strftime("%M")
-    if int(y[1])%5 == 0:
-        if run == 1:
-            if result:
-                cv2.imwrite("GeeksForGeeks.png", image)
-                img = cv2.imread("GeeksForGeeks.png")
-                box,label,count = cv.detect_common_objects(img)
-                count = 0
-                for i in label:
-                    if i == 'person':
-                        count += 1
-                print("there are " + str(count) + " people.")
-            else:
-                print("No image detected. Please! try again")
-            print(x)
-            run = 0
+    if result:
+        cv2.imwrite("818_room.png", image)
+        img = cv2.imread("818_room.png")
+        box,label,count = cv.detect_common_objects(img)
+        count = 0
+        for i in label:
+            if i == 'person':
+                count += 1
+        print("there are " + str(count) + " people.")
     else:
-        run = 1
+        print("No image detected. Please! try again")
+
+    engineer = {'Timestamp':x,'People':count}
+
+    result = messenger.put('/818 room','CCTV',engineer)
+
+    print("Engineer 1", result)
+    time.sleep(300)
